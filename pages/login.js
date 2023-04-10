a175729e7df606c00135d4fa50b1e6a10c4feed9
igor <gsambir519@gmail.com>
2023-04-10 17:29:21 +0300

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,44 +1,53 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import client from '../lib/axios';
+import styles from "../styles/components/login.module.css";
 
-const Login = () => {
-  const router = useRouter();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Ваш код для надсилання запиту на сервер для перевірки введеного логіну та пароля
-    // Якщо логін та пароль введені вірно, можна виконати:
-    router.push('/dashboard');
-  };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await client.post("/login", JSON.stringify({
+                username: username,
+                password: password
+            }))
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-    <div>
-      <h1>Вхід до системи</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Ім'я користувача:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <label className={styles.label}>
+                    Username:
+                    <input
+                        name="username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        className={styles.input}
+                    />
+                </label>
+                <label className={styles.label}>
+                    Password:
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        className={styles.input}
+                    />
+                </label>
+                <button type="submit" className={styles.button}>
+                    Log In
+                </button>
+            </form>
         </div>
-        <div>
-          <label htmlFor="password">Пароль:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Увійти</button>
-      </form>
-    </div>
-  );
-};
+    );
+}
 
 export default Login;
